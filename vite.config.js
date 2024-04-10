602f8b4c2571110c7ac219f468644910a48e7ffc
unknown <arteagawilliam13@gmail.com>
2024-04-10 16:29:13 -0400

    primier commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// prueba
 export default defineConfig({
   plugins: [react()],
-    base: "https://github.com/profWilliamArte/reactapithemoviedb1",
+    base: "https://profWilliamArte.github.io/reactapithemoviedb1",
   })
